5707ec9e65326a9239ac0a73abce1a80fb0c97b3
thayekovic <thayannear@gmail.com>
2024-02-25 10:31:45 -0300

    Project created

---
+++ src/application/Programa.java
@@ -0,0 +1,15 @@
+package application;
+
+import chess.ChessMatch;
+
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
+	}
+
+}
